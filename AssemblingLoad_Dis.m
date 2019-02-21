cb7587fcafc0df50dfce577629a491169195d58e
smtavakkoli <47473622+smtavakkoli@users.noreply.github.com>
2019-02-21 17:39:44 +0330

    Update AssemblingLoad_Dis.m

--- AssemblingLoad_Dis.m
+++ AssemblingLoad_Dis.m
@@ -2,12 +2,10 @@ function [PointLoad,PointDis]=AssemblingLoad_Dis(Ndof,DOF,pointload,pointdis)
 PointLoad=zeros(Ndof,1);
 PointDis=nan(Ndof,1);
 for i=1:size(pointload,1)
-    pointload(i,isnan(pointload(i,2:4)))=0;
     index=(pointload(i,1)-1)*DOF+1:pointload(i,1)*DOF;
     PointLoad(index)=pointload(i,2:4)';
 end
 for i=1:size(pointdis,1)
-    pointdis(i,isnan(pointdis(i,2:4)))=0;
     index=(pointdis(i,1)-1)*DOF+1:pointdis(i,1)*DOF;
     PointDis(index)=pointdis(i,2:4)';
 end
